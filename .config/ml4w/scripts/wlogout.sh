7eac2f21654b558481d99de57ac223d10d5ed2d1
Jester Lumacad <mr.jesterlumacad@gmail.com>
2026-08-07 17:00:26 +0800

    fix: wlogout broke on hyprland 0.56 (margin calc + bg position)

--- .config/ml4w/scripts/wlogout.sh
+++ .config/ml4w/scripts/wlogout.sh
@@ -1,5 +1,2 @@
-res_w=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
-res_h=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
-h_scale=$(hyprctl -j monitors | jq '.[] | select (.focused == true) | .scale' | sed 's/\.//')
-w_margin=$((res_h * 27 / h_scale))
-wlogout -b 5 -T $w_margin -B $w_margin
+w_margin=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height * 27 / (.scale * 100) | floor')
+wlogout -b 5 -T "$w_margin" -B "$w_margin"
